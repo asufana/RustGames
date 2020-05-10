9d015b95a9c86024eec4c8b022a7bc4243b52933
hana <asufanah@gmail.com>
2020-05-10 13:24:43 +0900

    [feature/#7_tetris] ミノの定義と表示

--- tetris/src/board.rs
+++ tetris/src/board.rs
@@ -1,9 +1,12 @@
+use crate::mino::{MINO_WIDTH, MINO_HEIGHT, Mino};
 
 pub const BOARD_HEIGHT: usize = 14;
 pub const BOARD_WIDTH: usize = 12;
 
 pub struct Board {
     field: [[usize; BOARD_WIDTH]; BOARD_HEIGHT],
+    buffer: [[usize; BOARD_WIDTH + MINO_WIDTH]; BOARD_HEIGHT + MINO_HEIGHT],
+    mino: Mino,
 }
 
 impl Board {
@@ -16,10 +19,21 @@ impl Board {
             }
         }
     }
+    //ミノの全フィールドに対して何らか処理を行う
+    fn apply_mino_fields<F>(&mut self, mut function: F)
+        where F: FnMut(&mut Board, usize, usize) -> () {
+        for y in 0..MINO_HEIGHT {
+            for x in 0..MINO_WIDTH {
+                function(self, x, y);
+            }
+        }
+    }
 
     fn new() -> Self {
         Self {
             field: [[0; BOARD_WIDTH]; BOARD_HEIGHT],
+            buffer: [[0; BOARD_WIDTH + MINO_WIDTH]; BOARD_HEIGHT + MINO_HEIGHT],
+            mino: Mino::random(),
         }
     }
 
@@ -33,6 +47,11 @@ impl Board {
                 board.set_field(x, y, 1);
             }
         });
+
+        //ミノ描画
+        this.apply_mino_fields(|board: &mut Board, x: usize, y: usize| {
+            board.set_buffer_field(x + board.mino.x, y + board.mino.y, board.mino.value(x, y));
+        });
         this
     }
 
@@ -44,13 +63,15 @@ impl Board {
     //アクセサ
     fn get_field(&self, x: usize, y: usize) -> usize { self.field[y][x] }
     fn set_field(&mut self, x: usize, y: usize, value: usize) { self.field[y][x] = value; }
+    fn get_buffer_field(&self, x: usize, y: usize) -> usize { self.buffer[y][x] }
+    fn set_buffer_field(&mut self, x: usize, y: usize, value: usize) { self.buffer[y][x] = value; }
 
     //描画
     pub fn output(&mut self) -> String {
         let mut output = String::new();
         for y in 0..BOARD_HEIGHT {
             for x in 0..BOARD_WIDTH {
-                let n = self.get_field(x, y);
+                let n = self.get_field(x, y) + self.get_buffer_field(x, y);
                 output = if n != 0 {
                     format!("{}🔳", output)
                 } else {
