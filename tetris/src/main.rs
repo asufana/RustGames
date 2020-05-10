08ce105d8b9c3c3001aa5796d9f57ff2e0734773
hana <asufanah@gmail.com>
2020-05-10 14:03:27 +0900

    [feature/#7_tetris] ミノを毎秒落下させる

--- tetris/src/main.rs
+++ tetris/src/main.rs
@@ -1,7 +1,7 @@
 use pancurses::{initscr, noecho, endwin};
 use tetris::board::Board;
 use std::thread::sleep;
-use std::time::Duration;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use device_query::{DeviceQuery, DeviceState, Keycode};
 
 fn main() {
@@ -23,6 +23,7 @@ fn main() {
     window.printw(board.output());
     window.refresh();
 
+    let mut time = get_seconds();
     loop {
         //入力受付
         let keys: Vec<Keycode> = device_state.get_keys();
@@ -40,5 +41,24 @@ fn main() {
             window.refresh();
             sleep(Duration::from_millis(100));
         }
+
+        //毎秒ドロップする
+        let current_time = get_seconds();
+        if time != current_time {
+            time = current_time;
+
+            //ドロップして再描画
+            board.down();
+            window.clear();
+            window.printw(board.output());
+            window.refresh();
+        }
     }
+}
+
+//現在秒の取得
+fn get_seconds() -> u64 {
+    let start = SystemTime::now();
+    let since_the_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    since_the_epoch.as_secs()
 }
